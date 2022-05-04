db29017dfa27b092c13ff15eec1c0eb3b8c02cb4
Steven <steven19930326@gmail.com>
2022-05-04 11:31:02 +0800

    050422-am

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+//http://localhost:3000/users/
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //接收Ajax透過GET傳過來的資料
+  const name = req.query.name ? req.query.name : "Ajax";
+  res.send(`Hello ${name}!!`);
 });
+
+
+
 
 module.exports = router;
